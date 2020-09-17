85775d3de57d4985234dba2e6c136941b922641b
Dasha Lary <dk2800@nyu.edu>
2020-09-17 17:41:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ def self.find_or_create_by_name(name)
   # else 
   #   self.find_by_name(name)
   # end
-self.create_by_name(name) || self.find_by_name(name)
+self.find_by_name(name) || self.create_by_name(name)
 
 end
 
